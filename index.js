407acdf1c5f7239d9cec0f0190bc58d32632155f
Swatilekha Roy <swatilekha.roy4@gmail.com>
2022-01-23 12:56:39 +0530

    add speak-zee speech model

--- index.js
+++ index.js
@@ -1,49 +1,78 @@
-// Require
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const http = require('http').createServer(app)
-const io = require('socket.io')(http)
-const { Deepgram } = require('@deepgram/sdk')
-const axios = require('axios')
+// Imports
+require("dotenv").config({ path: ".env" });
+const express = require("express");
+const bodyparser = require("body-parser");
+const ejs = require("ejs");
+const { Deepgram } = require("@deepgram/sdk");
 
-// Configure
-app.use(express.static('public'))
-const deepgram = new Deepgram(process.env.DG_KEY)
-const deepgramLive = deepgram.transcription.live({ utterances: true })
+// Intialize the app
+const app = express();
 
-// Logic
-io.on('connection', socket => {
-  console.log(`Connected at ${new Date().toISOString()}`)
+// Body-parser middleware
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
 
-  socket.on('microphone-stream', data => {
-    deepgramLive.send(data)
-  })
+// Template engine
+app.set("view engine", "ejs");
 
-  socket.on('search', async () => {
-    console.log('search event', transcript)
-    const { data } = await axios({
-      method: 'GET',
-      url: `https://api.genius.com/search?q=${transcript}`,
-      headers: {
-        Authorization: `Bearer ${process.env.GENIUS_TOKEN}`
-      }
-    })
-    const topThree = data.response.hits.slice(0, 3)
+// Loading static files
+app.use(express.static("public"));
+app.use(express.static("views"));
 
-    socket.emit('result', topThree)
-  })
-})
+// Homepage rendering
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-let transcript = ''
-deepgramLive.addListener('transcriptReceived', data => {
-  const result = JSON.parse(data)
-  const utterance = result.channel.alternatives[0].transcript
-  if (result.is_final && utterance) {
-    transcript += ' ' + utterance
-    console.log(transcript)
-  }
-})
+// /** Your Deepgram API Key*/
+// const deepgramApiKey = "47ae7d40eae8a05537e2d38b5734ac43006ce8cc";
 
-// Run
-http.listen(3000, console.log(`Started at ${new Date().toISOString()}`))
+// /** Name and extension of the file you downloaded (e.g., sample.wav) */
+// const pathToFile = "audio.wav";
+
+// /** Initialize the Deepgram SDK */
+// const deepgram = new Deepgram(deepgramApiKey);
+
+// /** Create a websocket connection to Deepgram */
+// const deepgramSocket = deepgram.transcription.live({ punctuate: true });
+
+// /** Listen for the connection to open and begin sending */
+// deepgramSocket.addListener("open", () => {
+//   console.log("Connection opened!");
+
+//   /** Grab your audio file */
+//   const fs = require("fs");
+//   const contents = fs.readFileSync(pathToFile);
+
+//   /** Send the audio to the Deepgram API in chunks of 1000 bytes */
+//   const chunk_size = 1000;
+//   for (i = 0; i < contents.length; i += chunk_size) {
+//     const slice = contents.slice(i, i + chunk_size);
+//     deepgramSocket.send(slice);
+//   }
+
+//   /** Close the websocket connection */
+//   deepgramSocket.finish();
+// });
+
+// /** Listen for the connection to close */
+// deepgramSocket.addListener("close", () => {
+//   console.log("Connection closed.");
+// });
+
+// /**
+//  * Receive transcripts based on sent streams and
+//  * write them to the console
+//  */
+// deepgramSocket.addListener("transcriptReceived", (transcription) => {
+//   console.log(transcription);
+//   console.log(transcription.channel.alternatives.transcript);
+// });
+
+// Ports
+var PORT = process.env.PORT || 3000;
+
+// Running App on Port
+app.listen(PORT, () => {
+  console.log(`Server Running on Port ${PORT}`);
+});
