6b72514f0207f123d5b365ba9b6f2d81b8d8b940
Swatilekha Roy <swatilekha.roy4@gmail.com>
2022-01-23 17:14:56 +0530

    add image

--- public/js/speech.js
+++ public/js/speech.js
@@ -326,6 +326,8 @@ async function initstop() {
   document.querySelector("#speech-final-rep").style.display = "block";
   document.querySelector("#record-btn").innerHTML = "Start Recording";
   document.querySelector("#stop-record-btn").style.display = "none";
+
+  console.log(str_g);
 }
 
 // plotting the graph and making the speech report
@@ -524,3 +526,54 @@ function speechgraph(posesc, audiosc, imagesc, timeprint) {
     }
   });
 }
+
+/* Grammar Page Javascript */
+
+var str_g = "";
+
+navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+  console.log({ stream });
+  if (!MediaRecorder.isTypeSupported("audio/webm"))
+    return alert("Browser not supported");
+  const mediaRecorder = new MediaRecorder(stream, {
+    mimeType: "audio/webm",
+  });
+  const socket = new WebSocket("wss://api.deepgram.com/v1/listen", [
+    "token",
+    "47ae7d40eae8a05537e2d38b5734ac43006ce8cc",
+  ]);
+  socket.onopen = () => {
+    document.querySelector("#status").textContent = "Connected";
+    console.log({ event: "onopen" });
+    mediaRecorder.addEventListener("dataavailable", async (event) => {
+      if (event.data.size > 0 && socket.readyState == 1) {
+        socket.send(event.data);
+      }
+    });
+    mediaRecorder.start(1000);
+  };
+
+  socket.onmessage = (message) => {
+    const received = JSON.parse(message.data);
+    const transcript = received.channel.alternatives[0].transcript;
+
+    if (transcript && received.is_final) {
+      console.log(transcript);
+      document.querySelector("#transcript").textContent += transcript + " ";
+      str_g = str_g + " " + transcript;
+    }
+  };
+
+  console.log(str_g);
+  socket.onclose = () => {
+    console.log({ event: "onclose" });
+  };
+
+  socket.onerror = (error) => {
+    console.log({ event: "onerror", error });
+  };
+});
+
+// function grammar() {
+
+// }
